8275811065e8bb20b47523acf5fc5d270c726367
ganjianwei <ganzib4fun@163.com>
2016-11-29 18:12:30 +0800

    first commit

---
+++ src/main/java/com/ganzib/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.ganzib.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by GanZiB on 16/11/29.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
